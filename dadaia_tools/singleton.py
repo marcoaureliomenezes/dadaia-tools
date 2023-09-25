acdabd544c5707c171fb0c0813b37956f13a1e17
dadaia <marcoaurelioreislima@gmail.com>
2023-09-24 23:52:33 -0300

    Adding additional configurations to test kafka

--- dadaia_tools/singleton.py
+++ dadaia_tools/singleton.py
@@ -16,5 +16,3 @@ class SingletonMeta(type):
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
-    
-
--- tests/send_data_to_kafka.py
+++ tests/send_data_to_kafka.py
@@ -1,22 +1,30 @@
 import json
 import sys
 import time
+
 from kafka import KafkaProducer
 
+if __name__ == '__main__':
 
-
-if __name__ == "__main__":
-
-    conn_str = "localhost:9092"
+    conn_str = 'localhost:9092'
     partitioner = lambda key, all, available: 0
     json_serializer = lambda data: json.dumps(data).encode('utf-8')
-    producer = KafkaProducer(bootstrap_servers=conn_str, value_serializer=json_serializer, partitioner=partitioner)
+    producer = KafkaProducer(
+        bootstrap_servers=conn_str,
+        value_serializer=json_serializer,
+        partitioner=partitioner,
+    )
     topic_name = 'test_topic'
     size = 100
     for i in range(size):
-        value = {"tste_col": f"valuess_{i}"}
-        value_str = "test"
+        value = {'tste_col': f'valuess_{i}'}
+        value_str = 'test'
 
-        producer.send(topic_name, value=value_str, key=f'topic_{i}'.encode('utf-8'), partition=0)
-        print(f"Sent {i} messages")
+        producer.send(
+            topic_name,
+            value=value_str,
+            key=f'topic_{i}'.encode('utf-8'),
+            partition=0,
+        )
+        print(f'Sent {i} messages')
         time.sleep(0.001)
