d9f433ad312c3ec34a6a7ad63b283a33474c7d94
dadaia <marcoaurelioreislima@gmail.com>
2023-09-30 20:40:49 -0300

    KafkaAdmin updated and tested with coverage of 86%

--- dadaia_tools/kafka_admin.py
+++ dadaia_tools/kafka_admin.py
@@ -16,14 +16,11 @@ class KafkaAdminAPI(metaclass=SingletonMeta):
         self.connection_str = connection_str
         self.admin = KafkaAdminClient(bootstrap_servers=self.connection_str)
 
-    def create_idempotent_topic(
-        self,
-        topic_name,
-        num_partitions=1,
-        replication_factor=1,
-        overwrite=False,
-        topic_config=None,
-    ):
+    def create_idempotent_topic(self, topic_name, overwrite=False, topic_config={}):
+        num_partitions = topic_config.get('num_partitions', 1)
+        replication_factor = topic_config.get('replication_factor', 1)
+        if topic_config.get('num_partitions'): del topic_config['num_partitions']
+        if topic_config.get('replication_factor'): del topic_config['replication_factor']
         topic = NewTopic(
             name=topic_name,
             num_partitions=num_partitions,
@@ -51,10 +48,12 @@ class KafkaAdminAPI(metaclass=SingletonMeta):
         )
         self.admin.alter_configs([cfg_resource_update])
 
+
     def delete_topic(self, topic):
         admin = KafkaAdminClient(bootstrap_servers=self.connection_str)
         admin.delete_topics([topic])
         return 'TOPIC DELETED'
+
 
     def list_topics(self, show_internal=False):
         topics = self.admin.list_topics()
@@ -63,12 +62,15 @@ class KafkaAdminAPI(metaclass=SingletonMeta):
         topics = sorted(topics)
         return topics
 
+
     def describe_topic(self, topic):
         return self.admin.describe_topics([topic])[0]
+
 
     def get_topic_config(self, topic):
         cfg_resource = ConfigResource(ConfigResourceType.TOPIC, topic)
         return self.admin.describe_configs([cfg_resource])[0]
+
 
     def clear_topics(self):
         topics = self.list_topics()
@@ -76,4 +78,4 @@ class KafkaAdminAPI(metaclass=SingletonMeta):
         return self
 
     def get_topic_by_name(self, topic_name):
-        return self.admin.describe_topics([topic_name])
+        return self.admin.describe_topics([topic_name])[0]
